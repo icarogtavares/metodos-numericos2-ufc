c12299f951c52a1cc91f044b52c380ce15d17aa2
icarotavaresxper <desenvolvimento@xper.social>
2019-03-19 03:16:09 -0300

    Adicionado método de integração genérico

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const math = require('mathjs');
 const Promise = require('bluebird');
 const readlineSync = require('readline-sync');
+const fechada = require('./fechada');
 
 const input = {};
 
@@ -26,10 +27,81 @@ function askForInputs (input) {
   input.fa = askAndReturnFilosofia();
   input.grau = askAndReturnGrau(input.fa);
   input.tolerancia = readlineSync.questionFloat('Qual é a tolerância? ');
+  input.funcao = x => x * x;
 
   return input;
 }
 
+/** Função para integração
+    @name functionToIntegrate
+    @function
+    @param {Number} value Valor a ser calculado
+*/
+
+/**
+ * Calcula a integral
+ * @param {Object} input Input do usuário
+ * @param {Number} input.limiteInferior Limite inferior da integral
+ * @param {Number} input.limiteSuperior Limite superior da integral
+ * @param {Number} input.fa Filosofia
+ * @param {Number} input.grau Grau da filosofia
+ * @param {Number} input.tolerancia Tolerância do erro
+ * @param {functionToIntegrate} input.funcao
+ */
+function integrate (input) {
+  let particao = 1;
+  let integralAntiga = 0;
+  while (true) {
+    const deltax = (input.limiteSuperior - input.limiteInferior) / particao;
+    let integralAtual = 0;
+    for (let i = 0; i < particao; i += 1) {
+      const xInicial = input.limiteInferior + (i * deltax);
+      const xFinal = xInicial + deltax;
+      switch (input.fa) {
+        case FA.FILOSOFIA_FECHADA:
+          switch (input.grau + 1) {
+            case 1:
+              integralAtual += fechada.funcaoGrauUm(input.funcao, xInicial, xFinal);
+              break;
+            case 2:
+              break;
+            default:
+              break;
+          }
+          break;
+        case FA.FILOSOFIA_ABERTA:
+          break;
+        default:
+          break;
+      }
+    }
+    const erro = math.abs(integralAtual - integralAntiga);
+    if (erro <= input.tolerancia) {
+      const resultado = {
+        resultado: integralAtual,
+        erro,
+      };
+      return resultado;
+    }
+
+    integralAntiga = integralAtual;
+    particao *= 2;
+  }
+}
+
+/**
+ * Imprime o resultado
+ * @param {Object} resultado
+ * @param {Number} resultado.resultado Resultado da integral
+ * @param {Number} resultado.erro Último erro obtido
+ */
+function finish (resultado) {
+  console.log('\n\n');
+  console.log('Resultado: ', resultado.resultado);
+  console.log('Erro: ', resultado.erro);
+}
+
 Promise.resolve(input)
   .then(askForInputs)
-  .then();
+  .then(integrate)
+  .then(finish);
