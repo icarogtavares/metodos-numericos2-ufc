2bf10b45236454803b6119c1786e92263ac97b46
icarotavaresxper <desenvolvimento@xper.social>
2019-03-19 20:43:29 -0300

    Removida mathjs de funcao fechada grau dois

--- src/fechada.js
+++ src/fechada.js
@@ -1,28 +1,10 @@
-const {
-  chain,
-  add,
-  multiply,
-} = require('mathjs');
-
 function funcaoGrauUm (funcao, limiteInferior, limiteSuperior) {
   return (limiteSuperior - limiteInferior) / 2.0 * (funcao(limiteInferior) + funcao(limiteSuperior));
 }
 
 function funcaoGrauDois (funcao, limiteInferior, limiteSuperior) {
-  const h = chain(limiteSuperior)
-    .subtract(limiteInferior)
-    .divide(2.0)
-    .done();
-  return chain(h)
-    .divide(3.0)
-    .multiply(
-      add(
-        funcao(limiteInferior),
-        multiply(4, funcao(add(limiteInferior, h))),
-        funcao(limiteSuperior)
-      )
-    )
-    .done();
+  const h = (limiteSuperior - limiteInferior) / 2.0;
+  return h / 3.0 * (funcao(limiteInferior) + 4 * funcao(limiteInferior + h) + funcao(limiteSuperior));
 }
 
 module.exports = {
