f03b2d765d1882ebbbf79219735d6d52a15e0f2b
icarotavaresxper <desenvolvimento@xper.social>
2019-03-19 03:53:54 -0300

    Adicionada função de integração Fechada Grau 2 e organizada funcaoGrauUm

--- src/fechada.js
+++ src/fechada.js
@@ -1,13 +1,33 @@
-const { chain, add } = require('mathjs');
+const { chain, add, subtract, multiply } = require('mathjs');
 
 function funcaoGrauUm (funcao, limiteInferior, limiteSuperior) {
   return chain(limiteSuperior)
     .subtract(limiteInferior)
     .divide(2)
-    .multiply(add(funcao(limiteInferior), funcao(limiteSuperior)))
+    .multiply(
+      add(
+        funcao(limiteInferior),
+        funcao(limiteSuperior)
+      )
+    )
+    .done();
+}
+
+function funcaoGrauDois (funcao, limiteInferior, limiteSuperior) {
+  const h = subtract(limiteInferior - limiteSuperior);
+  return chain(h)
+    .divide(3)
+    .multiply(
+      add(
+        funcao(limiteInferior),
+        multiply(4, funcao(add(limiteInferior, h))),
+        funcao(limiteSuperior)
+      )
+    )
     .done();
 }
 
 module.exports = {
   funcaoGrauUm,
+  funcaoGrauDois,
 };
